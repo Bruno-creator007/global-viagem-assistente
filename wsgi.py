d974c0b68f389231500fe6d15c07813d159e8d66
Seu Nome <seu.email@exemplo.com>
2025-02-27 22:47:23 -0300

    fix: update gunicorn command and simplify wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Adiciona o diretório raiz ao PYTHONPATH
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
 from backend import create_app
 
 app = create_app()
